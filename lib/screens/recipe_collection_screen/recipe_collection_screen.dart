c7838de806615c0696caf7cc2c1497fec778af5c
aadityayadav17 <yadavaadi17@icloud.com>
2024-10-11 21:44:51 +1000

    Screen Size (8/n) - Recipe Collection Screen

--- lib/screens/recipe_collection_screen/recipe_collection_screen.dart
+++ lib/screens/recipe_collection_screen/recipe_collection_screen.dart
@@ -8,6 +8,19 @@ import 'package:binarybandits/screens/recipe_selection_screen/recipe_selection_s
 import 'package:binarybandits/screens/recipe_collection_screen/recipe_collection_detail_screen.dart';
 import 'package:binarybandits/screens/weekly_menu_screen/weekly_menu_screen.dart';
 
+// Proportional helper functions
+double proportionalWidth(BuildContext context, double size) {
+  return size * MediaQuery.of(context).size.width / 375;
+}
+
+double proportionalHeight(BuildContext context, double size) {
+  return size * MediaQuery.of(context).size.height / 812;
+}
+
+double proportionalFontSize(BuildContext context, double size) {
+  return size * MediaQuery.of(context).size.width / 375;
+}
+
 class RecipeCollectionPage extends StatefulWidget {
   @override
   _RecipeCollectionPageState createState() => _RecipeCollectionPageState();
@@ -39,15 +52,15 @@ class _RecipeCollectionPageState extends State<RecipeCollectionPage> {
       appBar: AppBar(
         backgroundColor: const Color.fromRGBO(245, 245, 245, 1),
         elevation: 0,
-        toolbarHeight: 60,
+        toolbarHeight: proportionalHeight(context, 60),
         automaticallyImplyLeading: false,
         leading: Padding(
-          padding: const EdgeInsets.only(left: 8.0),
+          padding: EdgeInsets.only(left: proportionalWidth(context, 8)),
           child: IconButton(
             icon: Image.asset(
               'assets/icons/screens/common/back-key.png',
-              width: 24,
-              height: 24,
+              width: proportionalWidth(context, 24),
+              height: proportionalHeight(context, 24),
             ),
             onPressed: () {
               Navigator.pop(context);
@@ -59,13 +72,17 @@ class _RecipeCollectionPageState extends State<RecipeCollectionPage> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
-            padding: const EdgeInsets.only(left: 16.0, top: 10.0, bottom: 16.0),
+            padding: EdgeInsets.only(
+              left: proportionalWidth(context, 16),
+              top: proportionalHeight(context, 10),
+              bottom: proportionalHeight(context, 16),
+            ),
             child: Text(
               "Recipe Collection",
               style: GoogleFonts.robotoFlex(
                 color: Colors.black,
                 fontWeight: FontWeight.w900,
-                fontSize: 32,
+                fontSize: proportionalFontSize(context, 32),
                 height: 0.9,
               ),
             ),
@@ -81,7 +98,7 @@ class _RecipeCollectionPageState extends State<RecipeCollectionPage> {
 
   Widget _buildRecipeList() {
     return ListView.builder(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      padding: EdgeInsets.symmetric(horizontal: proportionalWidth(context, 16)),
       itemCount: recipes.length,
       itemBuilder: (context, index) {
         return _buildRecipeItem(recipes[index]);
@@ -91,7 +108,11 @@ class _RecipeCollectionPageState extends State<RecipeCollectionPage> {
 
   Widget _buildRecipeItem(Recipe recipe) {
     return Padding(
-      padding: const EdgeInsets.only(left: 16.0, right: 16.0, bottom: 16.0),
+      padding: EdgeInsets.only(
+        left: proportionalWidth(context, 16),
+        right: proportionalWidth(context, 16),
+        bottom: proportionalHeight(context, 16),
+      ),
       child: GestureDetector(
         onTap: () {
           // Navigate to the new recipe collection detail screen
@@ -104,10 +125,10 @@ class _RecipeCollectionPageState extends State<RecipeCollectionPage> {
           );
         },
         child: Container(
-          height: 56,
+          height: proportionalHeight(context, 56),
           decoration: BoxDecoration(
             color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
+            borderRadius: BorderRadius.circular(proportionalWidth(context, 10)),
             boxShadow: [
               BoxShadow(
                 color: Colors.grey.withOpacity(0.2),
@@ -118,12 +139,15 @@ class _RecipeCollectionPageState extends State<RecipeCollectionPage> {
             ],
           ),
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            padding: EdgeInsets.symmetric(
+                horizontal: proportionalWidth(context, 16)),
             child: Align(
               alignment: Alignment.centerLeft,
               child: Text(
                 recipe.name,
-                style: GoogleFonts.robotoFlex(fontSize: 16),
+                style: GoogleFonts.robotoFlex(
+                  fontSize: proportionalFontSize(context, 16),
+                ),
               ),
             ),
           ),
@@ -172,32 +196,32 @@ class _RecipeCollectionPageState extends State<RecipeCollectionPage> {
         BottomNavigationBarItem(
           icon: Image.asset(
             'assets/icons/bottom_navigation/home-off.png',
-            width: 24,
-            height: 24,
+            width: proportionalWidth(context, 24),
+            height: proportionalHeight(context, 24),
           ),
           label: '',
         ),
         BottomNavigationBarItem(
           icon: Image.asset(
             'assets/icons/bottom_navigation/discover-recipe-off.png',
-            width: 22,
-            height: 22,
+            width: proportionalWidth(context, 22),
+            height: proportionalHeight(context, 22),
           ),
           label: '',
         ),
         BottomNavigationBarItem(
           icon: Image.asset(
             'assets/icons/bottom_navigation/grocery-list-off.png',
-            width: 24,
-            height: 24,
+            width: proportionalWidth(context, 24),
+            height: proportionalHeight(context, 24),
           ),
           label: '',
         ),
         BottomNavigationBarItem(
           icon: Image.asset(
             'assets/icons/bottom_navigation/weekly-menu-off.png',
-            width: 24,
-            height: 24,
+            width: proportionalWidth(context, 24),
+            height: proportionalHeight(context, 24),
           ),
           label: '',
         ),
